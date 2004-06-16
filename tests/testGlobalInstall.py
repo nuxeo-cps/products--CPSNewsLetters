16834ecb8ab745c00f9eca3a688f29d3adea643e
janguenot <devnull@localhost>
2004-06-16 15:04:26 +0000

    More tests

--- tests/testGlobalInstall.py
+++ tests/testGlobalInstall.py
@@ -24,6 +24,22 @@ class TestGlobalInstall(CPSNewsLettersTestCase.CPSNewsLettersTestCase):
         self.portal._setObject('installer', installer)
         self.portal.installer()
 
+    def testNewsLetterTypeFixtures(self):
+        # Test if the NewsLetter portal_type is well installed
+        ttool = self.portal.portal_types
+        self.assertEqual('NewsLetter' in ttool.objectIds(), 1)
+
+    def testSubscriptionsToolFixtures(self):
+        # Test if CPSNewsLetters has register all the compulsory parameters
+        # within portal_subscriptions
+        # If portal_subscriptions is not installed then we juste skip the tests
+        # since it's possible to use the CPSNewsLetters without this component
+
+        subscriptions_tool = getToolByName(self.portal, 'portal_subscriptions')
+
+        if subscriptions_tool is not None:
+            self.assertEqual('NewsLetter' in subscriptions_tool.getRenderedPortalTypes(), 1)
+
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(TestGlobalInstall))
