2a27dab17d527ce6823882b7b214a8c96c93d2a0
astefanescu <devnull@localhost>
2004-11-05 17:02:25 +0000

    added widgets to drive search criteria fields

--- skins/cps_newsletters/getCPSNewsLettersDocumentLayouts.py
+++ skins/cps_newsletters/getCPSNewsLettersDocumentLayouts.py
@@ -45,6 +45,97 @@ newsletter_layout = {
                 'size': 0,
             },
         },
+        'query_fulltext': {
+            'type': 'String Widget',
+            'data': {
+                'fields': ('query_fulltext'),
+                'is_i18n': 1,
+                'label_edit': 'label_search_full_text',
+                'display_width': 40,
+                'size_max': 100,
+            },
+        },
+        'query_title': {
+            'type': 'String Widget',
+            'data': {
+                'fields': ('query_title'),
+                'is_i18n': 1,
+                'label_edit': 'label_search_title',
+                'display_width': 20,
+                'size_max': 100,
+            },
+        },
+        'query_description': {
+            'type': 'String Widget',
+            'data': {
+                'fields': ('query_description'),
+                'is_i18n': 1,
+                'label_edit': 'label_search_description',
+                'display_width': 20,
+                'size_max': 100,
+            },
+        },
+        'folder': {
+            'type': 'String Widget',
+            'data': {
+                'fields': ('folder'),
+                'is_i18n': 1,
+                'label_edit': 'label_folder',
+                'display_width': 30,
+                'size_max': 100,
+            },
+        },
+        'nb_items': {
+            'type': 'Int Widget',
+            'data': {
+                'fields': ('nb_items'),
+                'is_i18n': 1,
+                'label_edit': 'label_nb_items',
+                'display_width': 3,
+            },
+        },
+        'sort_by': {
+            'type': 'Select Widget',
+            'data': {
+                'fields': ['sort_by'],
+                'is_i18n': 1,
+                'label_edit': 'label_sort_by',
+                'vocabulary': 'search_sort_results_by',
+                },
+            },
+        'query_status': {
+            'type': 'Select Widget',
+            'data': {
+                'fields': ['query_status'],
+                'is_i18n': 1,
+                'label_edit': 'label_search_status',
+                'vocabulary': 'navigation_filter_review_state',
+                },
+            },
+        'query_portal_type': {
+            'type': 'MultiSelect Widget',
+            'data': {
+                'fields': ['query_portal_type'],
+                'is_i18n': 1,
+                'label_edit': 'label_search_portal_type',
+                'vocabulary': 'navigation_filter_listing_ptypes',
+                'size': 10,
+                },
+            },
+        'search': {
+            'type': 'Search Widget',
+            'data': {
+                'fields': (),
+                'label_edit': 'label_search',
+                'help': 'help_newsletter_search_widget',
+                'is_i18n': 1,
+                'css_class': 'articleELink',
+                'widget_ids': ['query_fulltext',
+                               'query_title', 'query_description',
+                               'query_status', 'query_portal_type',
+                               'folder', 'nb_items', 'sort_by'],
+                },
+            }, 
     },
     'layout': {
         'style_prefix': 'layout_default_',
@@ -54,6 +145,8 @@ newsletter_layout = {
             [{'widget_id': 'mailBodyHeader', 'ncols': 1},
             ],
             [{'widget_id': 'results', 'ncols': 1},
+            ],
+            [{'widget_id': 'search', 'ncols': 1},
             ],
             [{'widget_id': 'mailBodyFooter', 'ncols': 1},
             ],
